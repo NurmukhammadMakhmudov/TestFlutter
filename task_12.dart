fdb26c8434f09d41e95b160113eab9d06cb30a8a
NurmukhammadMakhmudov <80941197+NurmukhammadMakhmudov@users.noreply.github.com>
2021-04-16 13:18:01 +0500

    Add files via upload

---
+++ task_12.dart
@@ -0,0 +1,28 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2578
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 \
+\
+void main() \{\
+\
+ Map<String,String> dataBase=\{\
+    'Nurmukhammad Makhmudov':'07.07.2000',\
+    'Shokhruh Suleymanov':'21.01.1999',\
+    'Maksidjon Azzamov':'22.11.1998'\
+    \};\
+  dataBase.forEach((key,value)=>print('$key : $value'));\
+  \
+  String input='Shokhruh Suleymanov';\
+  dataBase.forEach((key,value)\{\
+    if(key==input)\
+    \{print('$key : $value');\
+    \}\
+  \});\
+  \
+\}\
+\
+}
